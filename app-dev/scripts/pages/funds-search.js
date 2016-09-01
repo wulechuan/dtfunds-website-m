8f8197cfbd4ef081ef8385bc8c9719dbe24cd857
wulechuan <wulechuan@live.com>
2016-09-01 16:19:38 +0800

    scroll control ok

--- app-dev/scripts/pages/funds-search.js
+++ app-dev/scripts/pages/funds-search.js
@@ -1,2 +1,20 @@
 $(function () {
+	var childrenHaveBeenPinned = false;
+
+	var $pinnableParent = $('.filters-block.children-pinnable');
+
+	$(document).on('scroll', function () {
+		var y = window.scrollY;
+		if (!childrenHaveBeenPinned) {
+			if (y >= 10) {
+				childrenHaveBeenPinned = true;
+				$pinnableParent.addClass('children-pinned');
+			}
+		} else {
+			if (y < 9) {
+				childrenHaveBeenPinned = false;
+				$pinnableParent.removeClass('children-pinned');
+			}
+		}
+	});
 });
